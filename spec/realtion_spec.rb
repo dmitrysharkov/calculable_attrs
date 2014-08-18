803fa8fc6be417971f7be02ea7bb09585d2c911c
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2014-08-18 11:36:44 +0300

    Added tests for calculate_attrs(true) and calculate_attrs

--- spec/realtion_spec.rb
+++ spec/realtion_spec.rb
@@ -9,14 +9,37 @@ describe Calculable::ActiveRecord::Relation do
 
     context 'method works in proper way' do
       before do
-        3.times { |i| create(:account, tr_count: 10, tr_amount: 10 * (i + 1)) }
-        Account.calculable_attr(balance: 'SUM(amount)'){ Transaction.joins(:account).all }
+        3.times { |i| create(:account, tr_count: 10 * (i + 1), tr_amount: 10) }
+        Account.calculable_attr(balance: 'SUM(amount)', number_of_transactions: 'COUNT(*)') { Transaction.joins(:account).all }
       end
 
-      subject { Account.all.calculate_attrs(:balance) }
+      context 'one attribute' do
+        subject { Account.all.calculate_attrs(:balance) }
+        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
+        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+      end
 
-      it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
-      it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+      context 'two attributes' do
+        subject { Account.all.calculate_attrs(:balance, :number_of_transactions) }
+        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
+        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
+        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+      end
+
+      context 'true as parameter' do
+        subject { Account.all.calculate_attrs(true) }
+        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
+        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
+        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+      end
+
+
+      context 'no parameters' do
+        subject { Account.all.calculate_attrs }
+        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
+        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
+        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+      end
     end
 
   end
