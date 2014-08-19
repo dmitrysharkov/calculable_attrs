fc9b90e3488a9dca4213737282f24d8f943d8caf
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2014-08-20 00:35:15 +0300

    Refactores samples

--- spec/realtion_spec.rb
+++ spec/realtion_spec.rb
@@ -57,34 +57,43 @@ describe Calculable::ActiveRecord::Relation do
       end
 
       context 'when has many' do
+        let(:shared_scope) { User.includes(:accounts).order(:id) }
+
+        subject(:users_balances) { scope.map(&:balance) }
+        subject(:users_number_of_transactions) { scope.map(&:number_of_transactions) }
+
+        subject(:accounts_balances) { scope.map {|user| user.accounts.map(&:balance).sort } }
+        subject(:accounts_number_of_transactions) { scope.map {|user| user.accounts.map(&:number_of_transactions).sort } }
 
         shared_examples 'balance attribute works in proper way' do
-          it { expect(subject.map {|user| user.accounts.map(&:balance).sort } ).to eq [[100, 200, 300], [1000, 2000, 3000], [10000, 20000, 30000]] }
-          it { expect(lambda { subject.load }).to be_executed_sqls(3) }
+          it { expect(users_balances).to eq [600, 6000, 60000] }
+          it { expect(accounts_balances).to eq [[100, 200, 300], [1000, 2000, 3000], [10000, 20000, 30000]] }
+          it { expect(lambda { scope.load }).to be_executed_sqls(4) }
         end
 
         shared_examples 'balance and number_of_transactions attributes in proper way' do
           it_behaves_like 'balance attribute works in proper way'
-          it { expect(subject.map {|user| user.accounts.map(&:number_of_transactions).sort }).to eq [[10, 20, 30], [10, 20, 30], [10, 20, 30]] }
+          it { expect(users_number_of_transactions).to eq [60, 60, 60] }
+          it { expect(accounts_number_of_transactions).to eq [[10, 20, 30], [10, 20, 30], [10, 20, 30]] }
         end
 
         context 'one attribute' do
-          subject { User.includes(:accounts).calculate_attrs(accounts: :balance).order(:id) }
+          let(:scope) { shared_scope.calculate_attrs(:balance, accounts: :balance) }
           it_behaves_like 'balance attribute works in proper way'
         end
 
         context 'two attributes' do
-          subject { User.includes(:accounts).calculate_attrs(accounts: [:balance, :number_of_transactions]).order(:id) }
+          let(:scope) { shared_scope.calculate_attrs(:balance, :number_of_transactions, accounts: [:balance, :number_of_transactions]) }
           it_behaves_like 'balance and number_of_transactions attributes in proper way'
         end
 
         context 'true as parameter' do
-          subject { User.includes(:accounts).calculate_attrs(accounts: true).order(:id) }
+          let(:scope) { shared_scope.calculate_attrs(true, accounts: true) }
           it_behaves_like 'balance and number_of_transactions attributes in proper way'
         end
 
         context 'no parameters' do
-          subject { User.includes(:accounts).calculate_attrs(:accounts).order(:id) }
+          let(:scope) { shared_scope.calculate_attrs(true, :accounts) }
           it_behaves_like 'balance and number_of_transactions attributes in proper way'
         end
       end
