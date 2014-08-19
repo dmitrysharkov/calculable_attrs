95fcb65b509d973732c2e3246e81bd4282c41b6d
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2014-08-19 23:06:45 +0300

    Added more examples

--- spec/realtion_spec.rb
+++ spec/realtion_spec.rb
@@ -42,21 +42,49 @@ describe Calculable::ActiveRecord::Relation do
       end
     end
 
-    context 'method works in proper with subordinately objects' do
+    context 'context method works in proper with subordinately objects' do
       before do
         3.times do |ui|
           user = create(:user)
           3.times { |i| create(:account, user: user, tr_count: 10 * (i + 1), tr_amount: 10 ** (ui + 1)) }
         end
         Account.calculable_attr(balance: 'SUM(amount)', number_of_transactions: 'COUNT(*)') { Transaction.joins(:account).all }
+        User.calculable_attr(balance: 'SUM(amount)', number_of_transactions: 'COUNT(*)', foreign_key: 'accounts.user_id') { Transaction.joins(:account).all }
       end
 
-      context 'one attribute' do
-        subject { User.includes(:accounts).calculate_attrs(accounts: :balance).order(:id).to_a.map {|user| user.accounts.map(&:balance).sort } }
-        it { is_expected.to eq [[100, 200, 300], [1000, 2000, 3000], [10000, 20000, 30000]] }
-        it { expect(lambda { subject }).to be_executed_sqls(3) }
+      context 'when has many' do
+        context 'one attribute' do
+          subject { User.includes(:accounts).calculate_attrs(accounts: :balance).order(:id).to_a.map {|user| user.accounts.map(&:balance).sort } }
+          it { is_expected.to eq [[100, 200, 300], [1000, 2000, 3000], [10000, 20000, 30000]] }
+          it { expect(lambda { subject }).to be_executed_sqls(3) }
+        end
+
+        context 'two attributes' do
+          subject {
+            User.includes(:accounts).calculate_attrs(accounts: [:balance, :number_of_transactions])
+              .order(:id).to_a.map {|user| user.accounts.map { |acc| [acc.balance, acc.number_of_transactions] }.sort }
+          }
+          it { is_expected.to eq [[[100,10], [200,20], [300,30]], [[1000, 10], [2000,20], [3000,30]], [[10000,10], [20000,20], [30000,30]]] }
+          it { expect(lambda { subject }).to be_executed_sqls(3) }
+        end
       end
 
+      context 'when belongs to' do
+        context 'one attribute' do
+          subject { Account.includes(:user).calculate_attrs(user: :balance).order(:user_id).map {|acc| acc.user.balance}.sort }
+          it { is_expected.to eq [600, 600, 600, 6000, 6000, 6000, 60000, 60000, 60000] }
+          it { expect(lambda { subject }).to be_executed_sqls(3) }
+        end
+
+        context 'two attributes' do
+          subject {
+            Account.includes(:user).calculate_attrs(user: [:balance, :number_of_transactions]).order(:user_id)
+             .to_a.map {|acc| [acc.user.balance, acc.user.number_of_transactions] }
+          }
+          it { is_expected.to eq [[600,60], [600,60], [600,60], [6000, 60], [6000,60], [6000,60], [60000,60], [60000,60], [60000,60]] }
+          it { expect(lambda { subject }).to be_executed_sqls(3) }
+        end
+      end
     end
 
   end
