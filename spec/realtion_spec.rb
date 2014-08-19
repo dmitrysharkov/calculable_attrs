2895534d01890a426cc13442ba8df5e02a2447d3
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2014-08-19 23:18:05 +0300

    Refacored one attribute examples with shared examples

--- spec/realtion_spec.rb
+++ spec/realtion_spec.rb
@@ -7,42 +7,44 @@ describe Calculable::ActiveRecord::Relation do
       it { is_expected.to respond_to :calculate_attrs }
     end
 
-    context 'method works in proper way' do
+    describe 'without subordinately objects' do
       before do
         3.times { |i| create(:account, tr_count: 10 * (i + 1), tr_amount: 10) }
         Account.calculable_attr(balance: 'SUM(amount)', number_of_transactions: 'COUNT(*)') { Transaction.joins(:account).all }
       end
 
-      context 'one attribute' do
-        subject { Account.all.calculate_attrs(:balance) }
+      shared_examples 'balance attribute works in proper way' do
         it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
         it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+      end
+
+      shared_examples 'balance and  number_of_transactions attributes in proper way' do
+        it_behaves_like 'balance attribute works in proper way'
+        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
+      end
+
+      context 'one attribute' do
+        subject { Account.all.calculate_attrs(:balance) }
+        it_behaves_like  'balance attribute works in proper way'
       end
 
       context 'two attributes' do
         subject { Account.all.calculate_attrs(:balance, :number_of_transactions) }
-        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
-        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
-        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+        it_behaves_like 'balance and  number_of_transactions attributes in proper way'
       end
 
       context 'true as parameter' do
         subject { Account.all.calculate_attrs(true) }
-        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
-        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
-        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+        it_behaves_like 'balance and  number_of_transactions attributes in proper way'
       end
-
 
       context 'no parameters' do
         subject { Account.all.calculate_attrs }
-        it { expect(subject.map(&:balance).sort).to eq [100, 200, 300] }
-        it { expect(subject.map(&:number_of_transactions).sort).to eq [10, 20, 30] }
-        it { expect(lambda { subject.load }).to be_executed_sqls(2) }
+        it_behaves_like 'balance and  number_of_transactions attributes in proper way'
       end
     end
 
-    context 'context method works in proper with subordinately objects' do
+    describe 'with subordinately objects' do
       before do
         3.times do |ui|
           user = create(:user)
