1fdbc52ebf77e745c834bd6564585d3cd39d25a6
Dmitry Sharkov <dmitry.sharkov@gmail.com>
2014-09-05 21:45:54 +0300

    Fixed defect: removed call dublication

--- lib/calculable_attrs/active_record/relation.rb
+++ lib/calculable_attrs/active_record/relation.rb
@@ -31,7 +31,6 @@ module CalculableAttrs::ActiveRecord::Relation
           calculable_orig_exec_queries
         else
           wrap_with_left_joins_and_exec_queries
-          append_joinded_calculable_attrs
         end
         apped_calculable_attrs
         @records
